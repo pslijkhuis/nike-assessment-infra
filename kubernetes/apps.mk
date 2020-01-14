769d8323944abc71b0d8c02fea2632074b3ee2e0
Peter Slijkhuis <p.slijkhuis@gmail.com>
2020-01-14 09:49:07 +0100

    Add nginx ingress controller

--- kubernetes/apps.mk
+++ kubernetes/apps.mk
@@ -1,7 +1,8 @@
 APPS = \
        roles \
        airflow \
-       clusterautoscaler
+       clusterautoscaler \
+       nginx-ingress
 
 
 # Ref: https://github.com/helm/charts/tree/master/stable/airflow
@@ -9,3 +10,6 @@ AIRFLOW_CHART_VERSION=5.2.4
 
 # Ref: https://github.com/helm/charts/tree/master/stable/cluster-autoscaler
 CLUSTERAUTOSCALER_CHART_VERSION=6.2.0
+
+# Ref: https://github.com/helm/charts/tree/master/stable/nginx-ingress
+NGINXINGRESS_CHART_VERSION=1.29.1
