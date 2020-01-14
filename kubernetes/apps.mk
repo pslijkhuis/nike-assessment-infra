32ea39e5598a110eb798c782e1bdfdfa7faf0a05
Peter Slijkhuis <p.slijkhuis@gmail.com>
2020-01-14 09:18:33 +0100

    Add clusterautoscaler to the cluster

--- kubernetes/apps.mk
+++ kubernetes/apps.mk
@@ -1,6 +1,11 @@
 APPS = \
-       roles
+       roles \
+       airflow \
+       clusterautoscaler
 
 
 # Ref: https://github.com/helm/charts/tree/master/stable/airflow
 AIRFLOW_CHART_VERSION=5.2.4
+
+# Ref: https://github.com/helm/charts/tree/master/stable/cluster-autoscaler
+CLUSTERAUTOSCALER_CHART_VERSION=6.2.0
