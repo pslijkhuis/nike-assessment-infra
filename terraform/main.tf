b9e2981a0a9e0b3c1db717ddba8f612c3da7caac
Peter Slijkhuis <p.slijkhuis@gmail.com>
2020-01-16 13:19:51 +0100

    Remove S3 and Dynamo from Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,32 +5,6 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "pslijkhuis-terraform-state"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-# Locking
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket         = "pslijkhuis-terraform-state"
